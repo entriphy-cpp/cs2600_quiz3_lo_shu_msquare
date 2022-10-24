1c42a78c4dabdcbe44b6ec54ef816015410568c8
Matthew Finerty <mfinerty@cpp.edu>
2022-10-23 20:29:50 -0700

    Parse matrix values from user

--- loShuMagicSquare.c
+++ loShuMagicSquare.c
@@ -7,15 +7,34 @@ int isMagicSquare(int *square); // Checks if the square is a magic square
 void printSquare(int *square); // Formats and prints the square
 
 int main(int argc, char const *argv[]) {
+    int square[3][3];
+
     // Check argument count (should be equal to 10)
-
-    // Parse user values into int[][]
-
-    // If user does not provide any arguments:
-    // - Create matrix that is a magic square
-    // - Create matrix that is not a magic square
-    // - Create a randomized matrix until is a magic square is found
-
+    if (argc > 1) {
+        if (argc > 9) {
+            for (int i = 1; i < 10; i++) {
+                // Parse values into square
+                int idx = i - 1;
+                int value = atoi(argv[i]);
+                if (value < 1 || value > 9) {
+                    // User provided a value out of range or was not an integer
+                    printf("Invalid value: %s; exiting...\n", argv[i]);
+                    return EXIT_FAILURE;
+                }
+                square[idx / 3][idx % 3] = value;
+            }
+        } else {
+            // User provided arguments, but did not provide the amount of values required
+            printf("Not enough arguments provided (need 9 values; got %d).\n", argc - 1);
+            return EXIT_FAILURE;
+        }
+    } else {
+        // If user does not provide any arguments:
+        // - Create matrix that is a magic square
+        // - Create matrix that is not a magic square
+        // - Create a randomized matrix until is a magic square is found
+    }
+    
     return EXIT_SUCCESS;
 }
 
