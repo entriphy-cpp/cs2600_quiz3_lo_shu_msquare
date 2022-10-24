b41e383810285fe1b18dcff44fb998ac94d228e0
Matthew Finerty <mfinerty@cpp.edu>
2022-10-23 20:18:27 -0700

    Create initial code outline

---
+++ loShuMagicSquare.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int rowSum(int *square, int row); // Gets the sum of the specified row
+int columnSum(int *square, int column); // Gets the sum of the specified column
+int isMagicSquare(int *square); // Checks if the square is a magic square
+void printSquare(int *square); // Formats and prints the square
+
+int main(int argc, char const *argv[]) {
+    // Check argument count (should be equal to 10)
+
+    // Parse user values into int[][]
+
+    // If user does not provide any arguments:
+    // - Create matrix that is a magic square
+    // - Create matrix that is not a magic square
+    // - Create a randomized matrix until is a magic square is found
+
+    return EXIT_SUCCESS;
+}
+
+int rowSum(int *square, int row) {
+    return 0; // Placeholder return
+}
+
+int columnSum(int *square, int column) {
+    return 0; // Placeholder return
+}
+
+int isMagicSquare(int *square) {
+    // Sum each row
+
+    // Sum each column
+
+    // Sum both diagonals
+
+    // Check that all sums are equal to each other
+
+    // Return result
+    return 1; // Placeholder return
+}
+
+void printSquare(int *square) {
+    // Print square
+}
